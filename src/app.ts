5138f41cf2c2fa5eb7b85148f5e3482552163a35
Jesús Espínola Conde <jesusespinolaconde@gmail.com>
2022-11-15 19:12:19 +0100

    ADD the babylon app file with a simple sphere, light and camera

---
+++ src/app.ts
@@ -0,0 +1,53 @@
+import { ArcRotateCamera, Engine, HemisphericLight, Mesh, MeshBuilder, Scene, Vector3 } from "@babylonjs/core";
+import "@babylonjs/core/Debug/debugLayer";
+import "@babylonjs/inspector";
+import "@babylonjs/loaders/glTF";
+
+class App {
+    private _canvas: HTMLCanvasElement;
+    private _engine: Engine;
+    private _scene: Scene;
+
+    private _camera: ArcRotateCamera;
+
+    private _initialiseApp (): void {
+        // create the canvas html element and attach it to the webpage
+        this._canvas = document.createElement("canvas");
+        this._canvas.id = "canvasArea";
+
+        const div: HTMLElement = document.getElementById("main");
+        div.appendChild(this._canvas);
+
+        // initialize babylon engine and scene
+        this._engine = new Engine(this._canvas, true);
+        this._scene = new Scene(this._engine);
+    }
+
+    constructor() {
+        this._initialiseApp();
+
+        this._camera = new ArcRotateCamera("Camera", 0, 0, 2, Vector3.Zero(), this._scene);
+        this._camera.attachControl(this._canvas, true);
+
+        const light: HemisphericLight = new HemisphericLight("light1", new Vector3(0, 10, 0), this._scene);
+        const sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, this._scene);
+
+        // hide/show the Inspector
+        window.addEventListener("keydown", (ev) => {
+            // Ctrl+Alt+I
+            if (ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
+                if (this._scene.debugLayer.isVisible()) {
+                    this._scene.debugLayer.hide();
+                } else {
+                    this._scene.debugLayer.show();
+                }
+            }
+        });
+
+        // run the main render loop
+        this._engine.runRenderLoop(() => {
+            this._scene.render();
+        });
+    }
+}
+new App();
