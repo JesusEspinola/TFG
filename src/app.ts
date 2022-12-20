d07b88d5c531e7275722e904005f19966a86ef91
Jesús Espínola Conde <jesusespinolaconde@gmail.com>
2022-12-20 20:31:34 +0100

    RUN npm run format

--- src/app.ts
+++ src/app.ts
@@ -1,62 +1,74 @@
-import { ArcRotateCamera, Engine, GroundMesh, HemisphericLight, MeshBuilder, Scene, Vector3 } from "@babylonjs/core";
+import {
+	ArcRotateCamera,
+	Engine,
+	GroundMesh,
+	HemisphericLight,
+	MeshBuilder,
+	Scene,
+	Vector3
+} from "@babylonjs/core";
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
 
 class App {
-    private _canvas: HTMLCanvasElement;
-    private _engine: Engine;
-    private _scene: Scene;
+	private _canvas: HTMLCanvasElement;
+	private _engine: Engine;
+	private _scene: Scene;
 
-    private _camera: ArcRotateCamera;
+	private _camera: ArcRotateCamera;
 
-    private _initialiseApp (): void {
-        // create the canvas html element and attach it to the webpage
-        this._canvas = document.createElement("canvas");
-        this._canvas.id = "canvasArea";
+	private _initialiseApp(): void {
+		// create the canvas html element and attach it to the webpage
+		this._canvas = document.createElement("canvas");
+		this._canvas.id = "canvasArea";
 
-        const div: HTMLElement = document.getElementById("main");
-        div.appendChild(this._canvas);
+		const div: HTMLElement = document.getElementById("main");
+		div.appendChild(this._canvas);
 
-        // initialize babylon engine and scene
-        this._engine = new Engine(this._canvas, true);
-        this._scene = new Scene(this._engine);
-    }
+		// initialize babylon engine and scene
+		this._engine = new Engine(this._canvas, true);
+		this._scene = new Scene(this._engine);
+	}
 
-    constructor() {
-        this._initialiseApp();
+	constructor() {
+		this._initialiseApp();
 
-        this._camera = new ArcRotateCamera("Camera", 0, 0, 2, Vector3.Zero(), this._scene);
-        this._camera.attachControl(this._canvas, true);
+		this._camera = new ArcRotateCamera("Camera", 0, 0, 2, Vector3.Zero(), this._scene);
+		this._camera.attachControl(this._canvas, true);
 
-        const light: HemisphericLight = new HemisphericLight("light1", new Vector3(0, 10, 0), this._scene);
-        const ground: GroundMesh = MeshBuilder.CreateGroundFromHeightMap(
-            "ground",
-            "https://doc.babylonjs.com/img/how_to/HeightMap/heightMap.png",
-            {
-                width: 10,
-                height: 10,
-                subdivisions: 100
-            },
-            this._scene
-        );
+		const light: HemisphericLight = new HemisphericLight(
+			"light1",
+			new Vector3(0, 10, 0),
+			this._scene
+		);
+		const ground: GroundMesh = MeshBuilder.CreateGroundFromHeightMap(
+			"ground",
+			"https://doc.babylonjs.com/img/how_to/HeightMap/heightMap.png",
+			{
+				width: 10,
+				height: 10,
+				subdivisions: 100
+			},
+			this._scene
+		);
 
-        // hide/show the Inspector
-        window.addEventListener("keydown", (ev) => {
-            // Ctrl+Alt+I
-            if (ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
-                if (this._scene.debugLayer.isVisible()) {
-                    this._scene.debugLayer.hide();
-                } else {
-                    this._scene.debugLayer.show();
-                }
-            }
-        });
+		// hide/show the Inspector
+		window.addEventListener("keydown", (ev) => {
+			// Ctrl+Alt+I
+			if (ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
+				if (this._scene.debugLayer.isVisible()) {
+					this._scene.debugLayer.hide();
+				} else {
+					this._scene.debugLayer.show();
+				}
+			}
+		});
 
-        // run the main render loop
-        this._engine.runRenderLoop(() => {
-            this._scene.render();
-        });
-    }
+		// run the main render loop
+		this._engine.runRenderLoop(() => {
+			this._scene.render();
+		});
+	}
 }
 new App();
--- src/tree.ts
+++ src/tree.ts
@@ -1,27 +1,30 @@
 import { Color3, Mesh, MeshBuilder, StandardMaterial } from "@babylonjs/core";
 
-export class Tree{
-    private _trunk: Mesh;
-    private _top: Mesh;
-    private _trunkMaterial: StandardMaterial;
-    private _topMaterial: StandardMaterial;
+export class Tree {
+	private _trunk: Mesh;
+	private _top: Mesh;
+	private _trunkMaterial: StandardMaterial;
+	private _topMaterial: StandardMaterial;
 
-    public mesh: Mesh;
+	public mesh: Mesh;
 
-    constructor(trunkHeight: number, topDiameter: number) {
-        this._trunk = MeshBuilder.CreateCylinder("trunk", { height: trunkHeight, diameter: topDiameter / 2 });
-        this._top = MeshBuilder.CreateSphere("top", { diameter: topDiameter });
+	constructor(trunkHeight: number, topDiameter: number) {
+		this._trunk = MeshBuilder.CreateCylinder("trunk", {
+			height: trunkHeight,
+			diameter: topDiameter / 2
+		});
+		this._top = MeshBuilder.CreateSphere("top", { diameter: topDiameter });
 
-        this._trunkMaterial = new StandardMaterial("trunkMaterial");
-        this._trunkMaterial.diffuseColor = new Color3(0.5, 0.25, 0);
-        this._topMaterial = new StandardMaterial("topMaterial");
-        this._topMaterial.diffuseColor = new Color3(0.25, 1, 0.25);
+		this._trunkMaterial = new StandardMaterial("trunkMaterial");
+		this._trunkMaterial.diffuseColor = new Color3(0.5, 0.25, 0);
+		this._topMaterial = new StandardMaterial("topMaterial");
+		this._topMaterial.diffuseColor = new Color3(0.25, 1, 0.25);
 
-        this._trunk.material = this._trunkMaterial;
-        this._top.material = this._topMaterial;
+		this._trunk.material = this._trunkMaterial;
+		this._top.material = this._topMaterial;
 
-        this._top.position.y += trunkHeight / 3;
+		this._top.position.y += trunkHeight / 3;
 
-        this.mesh = Mesh.MergeMeshes([this._trunk, this._top], true, true, undefined, false, true);
-    }
+		this.mesh = Mesh.MergeMeshes([this._trunk, this._top], true, true, undefined, false, true);
+	}
 }
